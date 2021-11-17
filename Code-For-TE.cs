e41e8041892bd37809764cc828c4d97defcb1d2c
shiyanovg <shiyanovegor@gmail.com>
2021-11-17 22:11:11 +0300

    Script for creating measures for "Top N + others" pattern

--- Code-For-TE.cs
+++ Code-For-TE.cs
@@ -1,1 +1,21 @@
-// Test file for Git 2
+string ListOfTable = "" ;
+string TN = "TopN";
+string PN = "Product Names";
+
+ foreach(var table in Model.Tables) {
+     // table.Name.Output();
+    ListOfTable = ListOfTable + (table.DaxObjectName ) + ",";     
+ };
+
+bool TopNCheck = ListOfTable.IndexOf(TN) >= 0;
+bool ProductNamesCheck = ListOfTable.IndexOf(PN) >= 0;
+
+if (!TopNCheck || !ProductNamesCheck) {
+    // 
+    Error("No 'TopN' Parameter!" + "\n" + "No 'Product Names' Table!");
+    return; 
+    } else {
+        Error("TopN");
+        Error("'Product Names'");
+        return;
+    };
